d2eaf3432e237940954a2d1102858436b61ffa3e
gregoriB <brandon.gregori@gmail.com>
2024-06-16 13:06:08 -0400

    Remove target directory if already exists before creation

--- mdisk.sh
+++ mdisk.sh
@@ -2,12 +2,14 @@ path=$1
 optional_name=$2
 
 if [[ ! "$path" ]]; then
+  echo
+  echo "!!!!!!!!!!!!!!!!"
   echo "No mountable path specified"
   echo "Exiting"
   exit 1
 fi
 
-echo "Mounting $path"
+echo_arg "Mounting $path"
 prepared="${path%/}" # remove trailing slash
 last="${prepared##*/}"
 
@@ -17,6 +19,22 @@ else
   mount_path="/mnt/$optional_name"
 fi
 
+if [[ -d "$mount_path" ]]; then
+  sudo rm -rf $mount_path
+  echo "Existing directory $mount_path removed"
+fi
+
 sudo mkdir $mount_path
+echo "$mount_path directory created"
+
 sudo mount $path $mount_path
-echo "Mounted to $mount_path"
+echo
+echo "$path mounted to $mount_path"
+
+echo
+echo "Sanity check for $mount_path: "
+ls -a $mount_path
+
+echo
+echo "Have a nice day!!"
+echo
